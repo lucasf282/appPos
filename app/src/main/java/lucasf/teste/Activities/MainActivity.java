3590fdc5576b049cb21364b6a465ae86a70a391c
lucasf282 <lucasfsiqueira282@gmail.com>
2017-10-10 21:56:44 -0300

    tela cadastro

--- app/src/main/java/lucasf/teste/Activities/MainActivity.java
+++ app/src/main/java/lucasf/teste/Activities/MainActivity.java
@@ -1,7 +1,11 @@
 package lucasf.teste.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import lucasf.teste.R;
 
@@ -10,6 +14,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG);
         setContentView(R.layout.activity_main);
+
+        Button btn_sigin = (Button) findViewById(R.id.btn_sigin);
+        btn_sigin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG);
     }
 }
